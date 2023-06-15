62f3f8315499476db7599be4c92f8f61edadd8df
VladyslavHodoniuk <122757652+VladyslavHodoniuk@users.noreply.github.com>
2023-06-15 23:15:58 +0200

    Setup project v0.5

--- client/src/Components/Reviews/ReviewsCard.tsx
+++ client/src/Components/Reviews/ReviewsCard.tsx
@@ -10,11 +10,16 @@ const ReviewsCard = () => {
         <div className='reviewsCard-wrapper'>
             <span className='span-article'>Відгуки наших <br/>
                 клієнтів</span>
-            <div className='map-wrapper'>
-            {ReviewsData.map((reviewer) => (
-                <ReviewsCardItem id={reviewer.id} userName={reviewer.userName} images={reviewer.photo}
-                                 text={reviewer.text}/>
-            ))}
+            <div>
+                <div className='map-wrapper'>
+                    {ReviewsData.map((reviewer) => (
+                        <ReviewsCardItem id={reviewer.id} userName={reviewer.userName} images={reviewer.photo}
+                                         text={reviewer.text}/>
+                    ))}
+                </div>
+                <div className='button-block'>
+                    <WhiteButton text='Всі коментарі'/>
+                </div>
             </div>
         </div>
     );
